fc585bf7bfb22dbe217b3caa5f1c53b3e4e33076
Vitali Peil <vpeil@cpan.org>
2014-04-28 08:10:49 +0000

    Import of VPEIL/Catmandu-Exporter-Template-0.04 from CPAN.
    gitpan-cpan-distribution: Catmandu-Exporter-Template
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         VPEIL/Catmandu-Exporter-Template-0.04.tar.gz
    gitpan-cpan-author:       VPEIL
    gitpan-cpan-maturity:     released

--- lib/Catmandu/Exporter/Template.pm
+++ lib/Catmandu/Exporter/Template.pm
@@ -7,7 +7,7 @@ use Catmandu;
 use Template;
 use Moo;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 with 'Catmandu::Exporter';
 
@@ -18,42 +18,59 @@ my $ADD_TT_EXT = sub {
     is_string($tmpl) && $tmpl !~ /\.tt$/ ? "$tmpl.tt" : $tmpl;
 };
 
-has xml             => (is => 'ro');
-has template_before => (is => 'ro', coerce => $ADD_TT_EXT);
-has template        => (is => 'ro', coerce => $ADD_TT_EXT, required => 1);
-has template_after  => (is => 'ro', coerce => $ADD_TT_EXT);
+has xml             => ( is => 'ro' );
+has template_before => ( is => 'ro', coerce => $ADD_TT_EXT );
+has template        => ( is => 'ro', coerce => $ADD_TT_EXT, required => 1 );
+has template_after => ( is => 'ro', coerce => $ADD_TT_EXT );
+has start_tag      => ( is => 'ro' );
+has end_tag        => ( is => 'ro' );
+has tag_style      => ( is => 'ro' );
+has interpolate    => ( is => 'ro' );
+has eval_perl      => ( is => 'ro' );
 
 sub _tt {
+    my $self = shift;
     local $Template::Stash::PRIVATE = 0;
-    state $tt = Template->new({
+    my %opts = (
         ENCODING     => 'utf8',
         ABSOLUTE     => 1,
+        RELATIVE     => 1,
         ANYCASE      => 0,
         INCLUDE_PATH => Catmandu->roots,
-        VARIABLES    => { _roots  => Catmandu->roots,
-                          _root   => Catmandu->root,
-                          _config => Catmandu->config, },
-    });
+        VARIABLES    => {
+            _roots  => Catmandu->roots,
+            _root   => Catmandu->root,
+            _config => Catmandu->config,
+        },
+    );
+
+    my @fields = qw/tag_style start_tag end_tag interpolate eval_perl/;
+    map { $opts{ uc $_ } = $self->$_ if $self->$_; } @fields;
+
+    state $tt = Template->new(%opts);
 }
 
 sub _process {
-    my ($self, $tmpl, $data) = @_;
-    $self->_tt->process($tmpl, $data || {}, $self->fh)
-        || Catmandu::Error->throw(Template->error || "Template error");
+    my ( $self, $tmpl, $data ) = @_;
+    unless ( $self->_tt->process( $tmpl, $data || {}, $self->fh ) ) {
+        my $msg = "Template error";
+        $msg .= ": " . $self->_tt->error->info if $self->_tt->error;
+        Catmandu::Error->throw($msg);
+    }
 }
 
 sub add {
-    my ($self, $data) = @_;
-    if ($self->count == 0) {
+    my ( $self, $data ) = @_;
+    if ( $self->count == 0 ) {
         $self->fh->print($XML_DECLARATION) if $self->xml;
-        $self->_process($self->template_before) if $self->template_before;
+        $self->_process( $self->template_before ) if $self->template_before;
     }
-    $self->_process($self->template, $data);
+    $self->_process( $self->template, $data );
 }
 
 sub commit {
     my ($self) = @_;
-    $self->_process($self->template_after) if $self->template_after;
+    $self->_process( $self->template_after ) if $self->template_after;
 }
 
 =head1 NAME
@@ -63,17 +80,15 @@ Catmandu::Exporter::Template - a TT2 Template exporter in Catmandu style
 
 =head1 SYNOPSIS
 
-If you are new to Catmandu see L<Catmandu::Tutorial>.
-
     use Catmandu::Exporter::Template;
 
     my $exporter = Catmandu::Exporter::Template->new(
-				fix => 'myfix.txt'
-				xml => 1,
-				template_before => '<path>/header.xml' ,
-				template => '<path>/record.xml' ,
-				template_after => '<path>/footer.xml' ,
-		   );
+                fix => 'myfix.txt'
+                xml => 1,
+                template_before => '<path>/header.xml' ,
+                template => '<path>/record.xml' ,
+                template_after => '<path>/footer.xml' ,
+           );
 
     $exporter->add_many($arrayref);
     $exporter->add_many($iterator);
@@ -85,9 +100,24 @@ If you are new to Catmandu see L<Catmandu::Tutorial>.
 
     printf "exported %d objects\n" , $exporter->count;
 
+=head1 DESCRIPTION
+
+This L<Catmandu::Exporter> can be used to export records using
+L<Template Toolkit|Template::Manual>. If you are new to Catmandu 
+see L<Catmandu::Tutorial>.
+
 =head1 METHODS
 
+Catmandu::Exporter::Template derives from L<Catmandu::Exporter> with all of its
+methods (C<add>, C<add_many>, C<count>, and C<log>). The following methods are
+supported in addition:
+
 =head2 new(%opts)
+
+The only required argument is 'template' which points to a file to render for
+each exported object. Set the 'template_before' and 'template_before' to add
+output at the start and end of the export.  Optionally provide an 'xml'
+indicator to include a XML header. 
 
 =over
 
@@ -111,25 +141,31 @@ template_after: Optional. Append template.
 
 fix: Optional. Apply Catmandu fixes while exporting.
 
+=item *
+
+start_tag
+
+=item *
+
+end_tag
+
+=item *
+
+tag_style
+
+=item *
+
+interpolate
+
+=item *
+
+eval_perl
+
 =back
-
-Catmandu::Exporter::Template can be used to export data objects using
-L<Template Toolkit|Template::Manual>. The only required argument is 'template'
-which points to a file to render for each exported object. Set the
-'template_before' and 'template_before' to add output at the start and end of
-the export.  Optionally provide an 'xml' indicator to include a XML header. 
-
-=head2 add($hashref)
-
-Add data $hashref to the exporter.
 
 =head2 commit
 
 Commit all changes and execute the template_after if given.
-
-=head2 count
-
-Gives the number of records added to the exporter.
 
 =head1 AUTHOR
 
@@ -138,6 +174,8 @@ Nicolas Steenlant, C<< <nicolas.steenlant at ugent.be> >>
 =head1 CONTRIBUTOR
 
 Vitali Peil, C<< <vitali.peil at uni-bielefeld.de> >>
+
+Jakob Voss, C<< <jakob.voss at gbv.de> >>
 
 =head1 LICENSE
 
